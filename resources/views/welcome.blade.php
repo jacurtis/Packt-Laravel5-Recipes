b67694618bba232340f66a578ff4eb0084412318
DevMarketer <devmarketer@Jays-MacBook-Pro.local>
2017-07-24 15:05:53 -0600

    Added Navigation and organized blade files. This belongs in video 1.5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,21 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('template')
 
-        <title>Laravel</title>
+@section('name')
+  <div class="container">
 
-        <!-- Fonts -->
-        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-      <div class="container">
+    <div class="jumbotron">
+      <h1>Ask a Question!</h1>
+      <p>Ask any question you want to know and we will get the answers for you.</p>
+      <p><a href="#" class="btn btn-primary btn-lg" role="button">Ask Now</a>
+    </div>
 
-        <div class="jumbotron">
-          <h1>Ask a Question!</h1>
-          <p>Ask any question you want to know and we will get the answers for you.</p>
-          <p><a href="#" class="btn btn-primary btn-lg" role="button">Ask Now</a>
-        </div>
+    <h2>Recent Questions:</h2>
 
-        <h2>Recent Questions:</h2>
+    @foreach ($questions as $question)
+    <div class="well">
+      <p class="lead">{{ $question }}</p>
+    </div>
+    @endforeach
 
-        @foreach ($questions as $question)
-        <div class="well">
-          <p class="lead">{{ $question }}</p>
-        </div>
-        @endforeach
-
-      </div>
-    </body>
-</html>
+  </div>
+@endsection
