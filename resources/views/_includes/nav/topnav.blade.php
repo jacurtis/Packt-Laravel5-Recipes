8d67e6f113c655396c3ad6cdcf6cb57302e57ba2
J. Alexander Curtis <jalexandercurtis@gmail.com>
2017-07-24 15:49:21 -0600

    Added links to the navigation

--- resources/views/_includes/nav/topnav.blade.php
+++ resources/views/_includes/nav/topnav.blade.php
@@ -8,18 +8,18 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Laravel Answers</a>
+      <a class="navbar-brand" href="{{route('index')}}">Laravel Answers</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="{{ route('index') }}">Home</a></li>
-        <li><a href="#">Recent</a></li>
+        <li><a href="{{ route('questions.index') }}">Recent</a></li>
         <li><a href="#">Popular</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <a href="#" class="btn btn-primary" style="margin-top:5px;">Ask A Question</a>
+        <a href="{{route('questions.create')}}" class="btn btn-primary" style="margin-top:5px;">Ask A Question</a>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
