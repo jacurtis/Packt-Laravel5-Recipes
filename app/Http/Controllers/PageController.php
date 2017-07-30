c2c48e6231b1f61869dfc7b5f2e1c28accc8012a
J. Alexander Curtis <jalexandercurtis@gmail.com>
2017-07-30 14:49:29 -0600

    3.5 - Eager Loading relationships refactor:

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -23,7 +23,7 @@ class PageController extends Controller
 
   public function profile($id)
   {
-    $user = User::findOrFail($id);
+    $user = User::with(['questions', 'answers', 'answers.question'])->find($id);
     return view('profile')->with('user', $user);
   }
 }
